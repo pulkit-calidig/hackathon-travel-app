b7de55d68c0dd4ae49fe9dd66b85257016da5593
pulkit-calidig <pulkit.s@calidig.com>
2025-03-24 17:25:36 +0530

    API to packages & destinations

--- backend/src/routes/unAuthRoutes.js
+++ backend/src/routes/unAuthRoutes.js
@@ -1,7 +1,28 @@
 import { Router } from "express"
+import prisma from "../prismaClient.js"
 
 const router = Router()
 
-router.get("/", (req, res) => {})
+// get packages
+router.get("/packages", async (req, res) => {
+  try {
+    const packages = await prisma.package.findMany()
+    res.json({ packages })
+  } catch (err) {
+    console.error(err.message)
+    return res.status(503).json({ message: "Something went wrong !!" })
+  }
+})
+
+// get destinations
+router.get("/destinatons", async (req, res) => {
+  try {
+    const destinations = await prisma.destination.findMany()
+    res.json({ destinations })
+  } catch (err) {
+    console.error(err.message)
+    return res.status(503).json({ message: "Something went wrong !!" })
+  }
+})
 
 export default router
