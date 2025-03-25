7464da55c20bd2e7464c8ed846a6a57581b3feb0
pulkit-calidig <pulkit.s@calidig.com>
2025-03-25 18:26:06 +0530

    Booking, review routes added. Comments added. Task list updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ import express, { json } from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/authRoutes.js"
 import unAuthRoutes from "./routes/unAuthRoutes.js"
+import bookingRoutes from "./routes/bookingRoutes.js"
+import reviewRoutes from "./routes/reviewRoutes.js"
 import middleware from "./middleware/middleware.js"
 import cors from "cors"
 
@@ -13,6 +15,7 @@ const PORT = process.env.PORT || 2104
 // middleware
 app.use(json())
 
+// cors
 app.use(
   cors({
     origin: "http://localhost:5173", // url where project will run
@@ -23,9 +26,18 @@ app.use(
 // routes
 app.use("/auth", authRoutes)
 app.use("/", unAuthRoutes)
+app.use("/booking", middleware, bookingRoutes)
+app.use("/review", middleware, reviewRoutes)
 
+// starter
 app.listen(PORT, (req, res) => {
   console.info(`Server is running on ${PORT}`)
 })
 
 // : Setup prisma and docker ASAP to start working on apis : DONE
+// : Add destinations and packages to db : DONE
+// : Make destination and package apis : DONE
+// : Make booking api : DONE
+// : Make review api : DONE
+// TODO: Make wishlist api
+// TODO: Make payment api
