e07dcda5780916b574d5eb45a9ab4a667afa9845
pulkit-calidig <pulkit.s@calidig.com>
2025-03-29 23:34:41 +0530

    Changed for more checks and features acc to db

--- backend/src/routes/bookingRoutes.js
+++ backend/src/routes/bookingRoutes.js
@@ -21,6 +21,28 @@ router.post("/make", async (req, res) => {
         .status(404)
         .json({ message: "All fields are required for make a booking" })
 
+    // check destination and package is of same place
+    const [checkDestination, checkPackage] = await Promise.all([
+      prisma.destination.findFirst({ where: { id: destinationId } }),
+      prisma.package.findUnique({ where: { id: packageId } }),
+    ])
+
+    if (!checkDestination || !checkPackage) {
+      return res
+        .status(404)
+        .json({ message: "Invalid destination or package!" })
+    }
+
+    const checkCity = await prisma.city.findUnique({
+      where: { id: checkPackage.cityId },
+    })
+
+    if (!checkCity || checkDestination.name !== checkCity.name)
+      return res
+        .status(404)
+        .json({ message: "Destination doesn't match with selected package !!" })
+
+    // startDate needs to be in future
     if (startDate < new Date().toISOString())
       return res
         .status(401)
@@ -28,6 +50,23 @@ router.post("/make", async (req, res) => {
 
     if (req.userId !== parseInt(userId))
       return res.status(403).json({ message: "Unauthorized. Forbidden Access" })
+
+    // check if user alrdy has booking for same place
+    const userBookings = await prisma.booking.findMany({
+      where: { userId },
+    })
+
+    const upcomingBooking = userBookings.some(
+      (booking) =>
+        booking.destinationId === destinationId &&
+        new Date(booking.startDate) > new Date()
+    )
+
+    if (upcomingBooking)
+      return res.status(401).json({
+        message:
+          `You already have a trip planned, ID: ${userBookings[0].id}. Please cancel it to make a new one !!`,
+      })
 
     await prisma.booking.create({
       data: {
