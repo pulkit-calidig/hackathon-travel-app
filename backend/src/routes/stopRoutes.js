a2eb1e23911389ee58603daaca43b1ca08d34d95
pulkit-calidig <pulkit.s@calidig.com>
2025-04-11 19:13:42 +0530

    API created to add stop for a booking

---
+++ backend/src/routes/stopRoutes.js
@@ -0,0 +1,49 @@
+import { Router } from "express"
+import prisma from "../prismaClient.js"
+
+const router = Router()
+
+router.post("/add/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    const { name, description, location, latitude, longitude, order } = req.body
+
+    const booking = await prisma.booking.findUnique({
+      where: { id: parseInt(id) },
+    })
+
+    if (!booking)
+      return res
+        .status(404)
+        .json({ message: "No booking found with this id !!" })
+
+    const stop = await prisma.stop.findFirst({ where: { name } })
+
+    if (stop)
+      return res
+        .status(401)
+        .json({ message: "Stop for this location is already there !!" })
+
+    const addStop = await prisma.stop.create({
+      data: {
+        bookingId: parseInt(id),
+        name,
+        description,
+        location,
+        latitude,
+        longitude,
+        order,
+      },
+    })
+
+    if (!addStop || !addStop.id)
+      return res.status(401).json({ message: "Failed to create a stop !!" })
+
+    res.json({ message: "Stop has been added for the trip" })
+  } catch (err) {
+    console.error(err.message)
+    res.status(503).json({ message: "Something went wrong !!" })
+  }
+})
+
+export default router
